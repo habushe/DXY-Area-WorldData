3e8694d700bbd0bf7587981d99130a9343963521
habushe <44997151+habushe@users.noreply.github.com>
2020-03-06 15:51:48 +0800

    Update csv_conpute.py

--- csv_conpute.py
+++ csv_conpute.py
@@ -7,7 +7,7 @@ from openpyxl.styles import Alignment
 
 # 输入输出参数，根据需要设置
 input_file = "out1.csv"
-output_file = "data/out_3_5.csv"
+output_file = "data/out_3_6.csv"
 excel_file = "疫情数据分析.xlsx"  # "test.xlsx"
 b_export_excel = False
 
