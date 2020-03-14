f8eeccb56e102c51cdfe1a79874df59db2dc9b54
habushe <44997151+habushe@users.noreply.github.com>
2020-03-14 11:36:14 +0800

    3.9

--- csv_conpute.py
+++ csv_conpute.py
@@ -1,13 +1,13 @@
 
 from datetime import timedelta
-
+import time
 import pandas
 from openpyxl import load_workbook
 from openpyxl.styles import Alignment
 
 # 输入输出参数，根据需要设置
 input_file = "out1.csv"
-output_file = "data/out_3_9.csv"
+output_file = "data/out_3_12.csv"
 excel_file = "疫情数据分析.xlsx"  # "test.xlsx"
 b_export_excel = False
 
@@ -18,98 +18,108 @@ pandas.set_option('display.max_rows', None)
 # 设置value的显示长度为100，默认为50
 pandas.set_option('max_colwidth', 200)
 
-# ！！！ 根据需要选择合适的字符集
-try:
-    dataf = pandas.read_csv(input_file, encoding='UTF-8')
-except:
-    dataf = pandas.read_csv(input_file, encoding='gb2312')
+def Csvcomputerrun():
+    while True:
+        csvcomputer()
+        time.sleep(3600)
 
-# 计算增量 根据日期间隔计算
-
-dataf['日期'] = pandas.to_datetime(dataf['日期'], format='%Y-%m-%d')  # 1900 -> 2020
-
-df_t = dataf['日期']
-df_date = df_t.drop_duplicates()  # 去重 这个返回Series对象
-
-# dataf['新增确诊'] = dataf['确诊']
-dataf.insert(loc=5, column='新增确诊', value=0)
-dataf.insert(loc=6, column='新增治愈', value=0)
-dataf.insert(loc=7, column='新增死亡', value=0)
-
-# df_date = df_date.sort_values(ascending=False)
-min_date = df_date.min()
-cur_date = df_date.max()
-
-df_t = pandas.DataFrame()
-
-step = 0
-print('处理数据... ')
-for index, data in dataf.iterrows():
-    data2 = dataf.loc[
-            (dataf['地区'] == data['地区']) & (dataf['国家'] == data['国家']) & (dataf['日期'] == data['日期'] - timedelta(days=1)),
-            :]
-    # 判断后一天是否存在
-    data3 = dataf.loc[
-            (dataf['地区'] == data['地区']) & (dataf['国家'] == data['国家']) & (dataf['日期'] == data['日期'] + timedelta(days=1)),
-            :]
-
-    if data2.shape[0] > 0:
-        dataf.loc[index, '新增确诊'] = data['确诊'] - data2.iloc[0, :]['确诊']
-        dataf.loc[index, '新增治愈'] = data['治愈'] - data2.iloc[0, :]['治愈']
-        dataf.loc[index, '新增死亡'] = data['死亡'] - data2.iloc[0, :]['死亡']
-    else:
-        dataf.loc[index, '新增确诊'] = data['确诊']
-        dataf.loc[index, '新增治愈'] = data['治愈']
-        dataf.loc[index, '新增死亡'] = data['死亡']
-
-    if (data['日期'] != cur_date) & (data3.shape[0] == 0):  # 后一天如果没有数据，加一条冲抵数据
-        new = pandas.DataFrame({'地区': data['地区'],
-                                '国家':data['国家'],
-                                '确诊': data['确诊'],
-                                '治愈': data['治愈'],
-                                '死亡': data['死亡'],
-                                '新增确诊': 0 - data['确诊'],
-                                '新增治愈': 0 - data['治愈'],
-                                '新增死亡': 0 - data['死亡'],
-                                '日期': data['日期'] + timedelta(days=1)},
-                               pandas.Index(range(1)))
-        #            print(new.head())
-        df_t = df_t.append(new)
-    step += 1
-    if step % 2000 == 0:
-        print('处理数据条数: ' + str(step))
-#    print(data['日期'])  # 输出处理进度
-print('处理数据完毕 条数: ' + str(step))
-out = dataf.append(df_t, sort=False)
-
-# 日期矫正为前一天，因为当天是发布日期，数据是反映前一天日期
-# out['日期'] = out['日期'] - timedelta(days=1)
-
-print('输出csv文件 : ' + output_file)
-out.to_csv(output_file, encoding="utf_8_sig", index=False)  # 为保证excel打开兼容，输出为UTF8带签名格式
+def  csvcomputer():
 
 
-# 导出excel 可选
-if b_export_excel:
-    writer = pandas.ExcelWriter(excel_file, engine='openpyxl')
+    # ！！！ 根据需要选择合适的字符集
     try:
-        # 加载指定的excel文件
-        print('加载excel文件 : ' + excel_file)
-        writer.book = load_workbook(excel_file)
+        dataf = pandas.read_csv(input_file, encoding='UTF-8')
+    except:
+        dataf = pandas.read_csv(input_file, encoding='gb2312')
 
-        print('处理数据...')
-        sheet_name = '原始数据'
-        max_row = writer.book[sheet_name].max_row
+    # 计算增量 根据日期间隔计算
 
-        writer.book[sheet_name].delete_rows(2, max_row-1)
+    dataf['日期'] = pandas.to_datetime(dataf['日期'], format='%Y-%m-%d')  # 1900 -> 2020
 
-        # copy existing sheets
-        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
-    except FileNotFoundError:
-        pass
+    df_t = dataf['日期']
+    df_date = df_t.drop_duplicates()  # 去重 这个返回Series对象
 
-    out.to_excel(writer, sheet_name, startrow=1, index=False, header=False)
+    # dataf['新增确诊'] = dataf['确诊']
+    dataf.insert(loc=5, column='新增确诊', value=0)
+    dataf.insert(loc=6, column='新增治愈', value=0)
+    dataf.insert(loc=7, column='新增死亡', value=0)
 
-    print('输出excel文件' + excel_file)
-    writer.save()
+    # df_date = df_date.sort_values(ascending=False)
+    min_date = df_date.min()
+    cur_date = df_date.max()
 
+    df_t = pandas.DataFrame()
+
+    step = 0
+    print('处理数据... ')
+    for index, data in dataf.iterrows():
+        data2 = dataf.loc[
+                (dataf['地区'] == data['地区']) & (dataf['国家'] == data['国家']) & (dataf['日期'] == data['日期'] - timedelta(days=1)),
+                :]
+        # 判断后一天是否存在
+        data3 = dataf.loc[
+                (dataf['地区'] == data['地区']) & (dataf['国家'] == data['国家']) & (dataf['日期'] == data['日期'] + timedelta(days=1)),
+                :]
+
+        if data2.shape[0] > 0:
+            dataf.loc[index, '新增确诊'] = data['确诊'] - data2.iloc[0, :]['确诊']
+            dataf.loc[index, '新增治愈'] = data['治愈'] - data2.iloc[0, :]['治愈']
+            dataf.loc[index, '新增死亡'] = data['死亡'] - data2.iloc[0, :]['死亡']
+        else:
+            dataf.loc[index, '新增确诊'] = data['确诊']
+            dataf.loc[index, '新增治愈'] = data['治愈']
+            dataf.loc[index, '新增死亡'] = data['死亡']
+
+        if (data['日期'] != cur_date) & (data3.shape[0] == 0):  # 后一天如果没有数据，加一条冲抵数据
+            new = pandas.DataFrame({'地区': data['地区'],
+                                    '国家':data['国家'],
+                                    '确诊': data['确诊'],
+                                    '治愈': data['治愈'],
+                                    '死亡': data['死亡'],
+                                    '新增确诊': 0 - data['确诊'],
+                                    '新增治愈': 0 - data['治愈'],
+                                    '新增死亡': 0 - data['死亡'],
+                                    '日期': data['日期'] + timedelta(days=1)},
+                                   pandas.Index(range(1)))
+            #            print(new.head())
+            df_t = df_t.append(new)
+        step += 1
+        if step % 2000 == 0:
+            print('处理数据条数: ' + str(step))
+    #    print(data['日期'])  # 输出处理进度
+    print('处理数据完毕 条数: ' + str(step))
+    out = dataf.append(df_t, sort=False)
+
+    # 日期矫正为前一天，因为当天是发布日期，数据是反映前一天日期
+    # out['日期'] = out['日期'] - timedelta(days=1)
+
+    print('输出csv文件 : ' + output_file)
+    out.to_csv(output_file, encoding="utf_8_sig", index=False)  # 为保证excel打开兼容，输出为UTF8带签名格式
+
+
+    # 导出excel 可选
+    if b_export_excel:
+        writer = pandas.ExcelWriter(excel_file, engine='openpyxl')
+        try:
+            # 加载指定的excel文件
+            print('加载excel文件 : ' + excel_file)
+            writer.book = load_workbook(excel_file)
+
+            print('处理数据...')
+            sheet_name = '原始数据'
+            max_row = writer.book[sheet_name].max_row
+
+            writer.book[sheet_name].delete_rows(2, max_row-1)
+
+            # copy existing sheets
+            writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
+        except FileNotFoundError:
+            pass
+
+        out.to_excel(writer, sheet_name, startrow=1, index=False, header=False)
+
+        print('输出excel文件' + excel_file)
+        writer.save()
+
+if __name__ == '__main__':
+    Csvcomputerrun()
--- csvfiter.py
+++ csvfiter.py
@@ -1,99 +1,108 @@
 
 import pandas
 from datetime import timedelta
+import time
 
 input_file = "foreign/csv/foreignArea.csv"
 output_file = "out1.csv"
 
 
+def Csvtiterrun():
+    while True:
+        csvtiter()
+        time.sleep(3600)
 
-# pandas显示配置 方便调试
-# 显示所有列
-pandas.set_option('display.max_columns', None)
-# 显示所有行
-pandas.set_option('display.max_rows', None)
-# 设置value的显示长度为100，默认为50
-pandas.set_option('max_colwidth', 200)
+def  csvtiter():
 
-# ！！！ 根据需要选择合适的字符集
-try:
-    dataf = pandas.read_csv(input_file, encoding='UTF-8')
-except:
-    dataf = pandas.read_csv(input_file, encoding='gb2312')
+    # pandas显示配置 方便调试
+    # 显示所有列
+    pandas.set_option('display.max_columns', None)
+    # 显示所有行
+    pandas.set_option('display.max_rows', None)
+    # 设置value的显示长度为100，默认为50
+    pandas.set_option('max_colwidth', 200)
 
-
-
-dataf['updateTime'] = pandas.to_datetime(dataf['updateTime'])
-dataf['date'] = dataf['updateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
-dataf['date'] = pandas.to_datetime(dataf['date'])
-# print(type(dataf))  print(dataf.dtypes)   print(dataf.head())
-
-
-# 提取省列表
-df_t = dataf['provinceName']
-df_province = df_t.drop_duplicates()  # 去重 这个返回Series对象
-# df_province = df_t.unique()  # 去重 这个返回 ndarray
+    # ！！！ 根据需要选择合适的字符集
+    try:
+        dataf = pandas.read_csv(input_file, encoding='UTF-8')
+    except:
+        dataf = pandas.read_csv(input_file, encoding='gb2312')
 
 
-df = pandas.DataFrame(index=None)
 
-df_t = dataf['date']
-df_date = df_t.drop_duplicates()  # 去重 返回Series对象
-df_date = df_date.sort_values()
-for date_t in df_date:
-    for name in df_province:
-        print(date_t.strftime('%Y-%m-%d') + name)  # 输出处理进度
-        df1 = dataf.loc[(dataf['provinceName'].str.contains(name)) & (dataf['date'] == date_t), :]
+    dataf['updateTime'] = pandas.to_datetime(dataf['updateTime'])
+    dataf['date'] = dataf['updateTime'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    dataf['date'] = pandas.to_datetime(dataf['date'])
+    # print(type(dataf))  print(dataf.dtypes)   print(dataf.head())
 
-        df1 = df1.loc[(df1['updateTime'] == df1['updateTime'].max()), :]  # 筛出省的最后数据 避免之前时间的市数据干扰，产生孤立值
 
-        df_c = df1['countryName']
-        df_country = df_c.drop_duplicates()  # 去重 这个返回Series对象
-        confirmedCount = df1['confirmedCount'].max()
-        suspectedCount = df1['suspectedCount']
-        curedCount = df1['curedCount'].max()
-        deadCount = df1['deadCount'].max()
-       # updateTime2=df1['updateTime'].max()
+    # 提取省列表
+    df_t = dataf['provinceName']
+    df_province = df_t.drop_duplicates()  # 去重 这个返回Series对象
+    # df_province = df_t.unique()  # 去重 这个返回 ndarray
 
-        for country in df_country:
-            df2 = df1.loc[(df1['countryName'].str.contains(country)), :]  # df2筛选出某个市的数据
 
-            # 使用当天最后时间的数据，注释这行，则使用当天最大值提取数据
-            df2 = df2.loc[(df2['updateTime'] == df2['updateTime'].max()), :]
+    df = pandas.DataFrame(index=None)
 
-            new = pandas.DataFrame({'地区': name,
-                                    '国家':country,
-                                    '确诊': confirmedCount,
-                                    '疑似':suspectedCount,
-                                    '治愈': curedCount,
-                                    '死亡': deadCount,
-                                    '日期': date_t},
-                                    #'更新时间':updateTime2},
-                                   pandas.Index(range(1)))
-            #            print(new.head())
-            df = df.append(new)
+    df_t = dataf['date']
+    df_date = df_t.drop_duplicates()  # 去重 返回Series对象
+    df_date = df_date.sort_values()
+    for date_t in df_date:
+        for name in df_province:
+            print(date_t.strftime('%Y-%m-%d') + name)  # 输出处理进度
+            df1 = dataf.loc[(dataf['provinceName'].str.contains(name)) & (dataf['date'] == date_t), :]
 
-# 补齐一个地区的空数据
-# #
-for date_t in df_date:
-    #    print(date_t.strftime('%Y-%m-%d') + name)  # 输出处理进度
-    if date_t == df_date.max():  # 最后一天不处理
-        continue
-    date_add = date_t + timedelta(days=1)
-    for name in df_province:
-        df1 = df.loc[(df['地区'].str.contains(name)) & (df['日期'] == date_t), :]
-        if df1.shape[0] > 0:
-            df2 = df.loc[
-                  (df['地区'].str.contains(name)) & (df['日期'] == date_add),
-                  :]
-            if df2.shape[0] == 0:  # 后面一天省数据为空 把当前数据填到后一天
-                print('追加 ' + date_add.strftime('%Y-%m-%d') + name)  # 输出处理进度
+            df1 = df1.loc[(df1['updateTime'] == df1['updateTime'].max()), :]  # 筛出省的最后数据 避免之前时间的市数据干扰，产生孤立值
 
-                for index, data in df1.iterrows():  # 改变值 使用索引
-                    df1.loc[index, '日期'] = date_add
-                df = df.append(df1)
+            df_c = df1['countryName']
+            df_country = df_c.drop_duplicates()  # 去重 这个返回Series对象
+            confirmedCount = df1['confirmedCount'].max()
+            suspectedCount = df1['suspectedCount']
+            curedCount = df1['curedCount'].max()
+            deadCount = df1['deadCount'].max()
+           # updateTime2=df1['updateTime'].max()
 
-# print(df)
+            for country in df_country:
+                df2 = df1.loc[(df1['countryName'].str.contains(country)), :]  # df2筛选出某个市的数据
 
-df.to_csv(output_file, encoding="utf_8_sig", index=False)  # 为保证excel打开兼容，输出为UTF8带签名格式
+                # 使用当天最后时间的数据，注释这行，则使用当天最大值提取数据
+                df2 = df2.loc[(df2['updateTime'] == df2['updateTime'].max()), :]
 
+                new = pandas.DataFrame({'地区': name,
+                                        '国家':country,
+                                        '确诊': confirmedCount,
+                                        '疑似':suspectedCount,
+                                        '治愈': curedCount,
+                                        '死亡': deadCount,
+                                        '日期': date_t},
+                                        #'更新时间':updateTime2},
+                                       pandas.Index(range(1)))
+                #            print(new.head())
+                df = df.append(new)
+
+    # 补齐一个地区的空数据
+    # #
+    for date_t in df_date:
+        #    print(date_t.strftime('%Y-%m-%d') + name)  # 输出处理进度
+        if date_t == df_date.max():  # 最后一天不处理
+            continue
+        date_add = date_t + timedelta(days=1)
+        for name in df_province:
+            df1 = df.loc[(df['地区'].str.contains(name)) & (df['日期'] == date_t), :]
+            if df1.shape[0] > 0:
+                df2 = df.loc[
+                      (df['地区'].str.contains(name)) & (df['日期'] == date_add),
+                      :]
+                if df2.shape[0] == 0:  # 后面一天省数据为空 把当前数据填到后一天
+                    print('追加 ' + date_add.strftime('%Y-%m-%d') + name)  # 输出处理进度
+
+                    for index, data in df1.iterrows():  # 改变值 使用索引
+                        df1.loc[index, '日期'] = date_add
+                    df = df.append(df1)
+
+    # print(df)
+
+    df.to_csv(output_file, encoding="utf_8_sig", index=False)  # 为保证excel打开兼容，输出为UTF8带签名格式
+
+if __name__ == '__main__':
+    Csvtiterrun()
